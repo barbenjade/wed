5963f17c66aa4ae4714065c221a90e9235d83cb9
kassandrashort <re.kassandrashort@gmail.com>
2019-05-10 11:03:27 -0600

    test commit

--- read.php
+++ read.php
@@ -14,21 +14,39 @@ $results = mysqli_query($connection, "SELECT * FROM users");
 
 echo "<table id=table1 border=1>
 <tr>
+<form method=POST action=update1.php>
 <th>ID</th>
 <th>Name</th>
 <th>Password</th>
+</form>
 </tr>";
 while($row = mysqli_fetch_assoc($results)){
     echo "<tr>";
+    echo "<form method=POST action=update1.php>";
     echo "<td>" . "<input type=text name=id value=" . $row['id'] . ">" . "</td>";
     echo "<td>" . "<input type=text name=name value=" . $row['name'] . ">" . "</td>";
     echo "<td>" . "<input type=text name=password value=" . $row['password1'] . ">" . "</td>";
-    echo "<td>" . "<input type=button name=update class=button1 value=Update" . ">" . "</td>";
+    echo "<td>" . "<input type=button name=updateN class=button1 value=Update" . ">" . "</td>";
     echo "<td>" . "<input type=button name=delete class=button1 value=Delete" . ">" . "</td>";
+    echo "</form>";
     echo "</tr>";
 }
 echo "</table>";
 
+
+if(isset($_POST['updateN'])){
+    $username = $_POST['user_name'];
+    $password = $_POST['password'];
+
+    $query = "UPDATE users SET ";
+    $query .= "name = '$username'";
+    $query .= "password1 = '$password";
+    $query .= "WHERE id = $ID";
+
+    $results = mysqli_query($query, $connection);
+
+    echo "yes I do";
+}
 ?>
 
 <html>
@@ -60,7 +78,9 @@ input {
 
     <input type="submit" name="submit" id="submit"> 
 </form> -->
-
+<div class=echo1>
+    <?php updateTwo("Shrek is one"); ?>
+</div>
 <div id="link1">
     <a href="crud.php">Home Page</a>
 </div> 
